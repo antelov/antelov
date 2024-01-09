3c4385d29d2aa2c151be7253ae1c554c610dca11
ali <ali.missaoui.jr@gmail.com>
2024-01-09 00:58:52 +0000

    "token sending implementation"

--- antelovServer/core/urls.py
+++ antelovServer/core/urls.py
@@ -3,13 +3,34 @@ from core.views import (
     CustomerView,
     ServiceProviderView,
 )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 
 
-
+#customer APIs
 urlpatterns = [
     path('customer/', CustomerView.as_view(), name='customer_api'),
-    path('serviceprovider/', ServiceProviderView.as_view(), name='service_provider_api'),
     # path('login/',LoginView.as_view(),name="login"),
     # path('logout/',LogoutView.as_view(),name="logout"),
+]
+
+
+#service provider APIs
+urlpatterns += [
+    path('serviceprovider/', ServiceProviderView.as_view(), name='service_provider_api'),
+
+]
+
+
+
+#token APIs
+urlpatterns += [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
 ]
--- antelovServer/core/views.py
+++ antelovServer/core/views.py
@@ -45,42 +45,15 @@ class ServiceProviderView(APIView):
         serializer = ServiceProviderSerializer(data=data)
         
         if serializer.is_valid():
-            user = User.objects.create_user(data.get("user"), data.get("email"), data.get("password"))
+            print(serializer)
+            user = userModel.objects.create_user(data.get("username"), data.get("email"), data.get("password"),type_user="service provider")
             data['user']=user.id
-            serializer.save()
-            return Response({"message": "Customer registered successfully"}, status=status.HTTP_201_CREATED)
+            serializer = ServiceProviderSerializer(data=data)
+            # serializer.is_valid()
+            if serializer.is_valid():
+                print(serializer)
+                serializer.save()
+                return Response({"message": "Service Provider registered successfully"}, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# class LoginView(APIView):
-#     def post(self, request):
-#         username_or_email = request.data.get('username_or_email')
-#         password = request.data.get('password')
-#         user_type = request.data.get('user_type')  
-
-#         if user_type == 'customer':
-#             try:
-#                 user = Customer.objects.get(email=username_or_email)
-#             except Customer.DoesNotExist:
-#                 user = None
-#         elif user_type == 'pending_account':
-#             try:
-#                 user = PendingAccounts.objects.get(email=username_or_email)
-#             except PendingAccounts.DoesNotExist:
-#                 user = None
-#         else:
-#             return Response({"message": "Invalid user type"}, status=status.HTTP_400_BAD_REQUEST)
-
-#         if user and check_password(password, user.password):
-#             refresh = RefreshToken.for_user(user)
-#             response = Response()
-#             response.set_cookie(key='access_token', value=str(refresh.access_token), httponly=True)
-#             return response
-
-#         return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-
-# class LogoutView(APIView):
-#     def post(self, request):
-#         response = Response()
-#         response.delete_cookie('access_token')
-#         return response
